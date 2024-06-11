19231b926d50062daf11477355d618415c518dc0
selcia25 <selcia2110605@ssn.edu.in>
2024-06-11 10:28:05 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './home/home.module';
+import { ReservationModule } from './reservation/reservation.module';
+
 @NgModule({
   declarations: [
     AppComponent
